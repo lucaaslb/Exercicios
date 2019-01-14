1578e7a41d12b4d3a3bcafb23a8b964de7d29972
Lucas Lacerda <lucaaslb@hotmail.com>
2019-01-14 20:43:10 -0200

    Solução do exercicio 4

---
+++ exercicio-04/lucaaslb/exercicio.py
@@ -0,0 +1,30 @@
+import csv
+
+with open('pessoas.csv', 'r') as f:   
+    reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
+    next(reader, None) # skip header
+
+    lines = 0
+    masc = 0
+    fem = 0
+    dMaior = 0
+    dMenor = 0
+
+    for row in reader:        
+        lines += 1
+        if row[2] == 'M':
+            masc += 1
+        elif row[2] == 'F':
+            fem += 1
+
+        if int(row[1]) >= 18:
+            dMaior +=1
+        elif int(row[1]) < 18:
+            dMenor +=1
+
+        print(row)
+
+    print(masc*100 / lines) #porcentagem de homens
+    print(fem*100 / lines)  #porcentagem de mulheres 
+    print(dMaior)
+    print(dMenor) 
